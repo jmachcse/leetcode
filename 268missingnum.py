b20dafc51cfcf4d6cdb4591fad1ac85d69662425
jmachcse <mach.31@buckeyemail.osu.edu>
2024-01-09 15:26:32 -0500

    first success

--- 268missingnum.py
+++ 268missingnum.py
@@ -4,3 +4,13 @@ class Solution(object):
 		:type nums: List[int]
 		:rtype: int
 		"""
+
+		if 0 not in nums:
+			return 0
+		nums.sort()
+		current = nums[0]
+		for num in nums:
+			if current + 1 in nums:
+				current += 1
+			else:
+				return current + 1
