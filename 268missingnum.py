39eebfbbbf56268cd691f44285908a8f31d9fa26
jmachcse <mach.31@buckeyemail.osu.edu>
2024-01-09 15:10:23 -0500

    fix spacing

--- 268missingnum.py
+++ 268missingnum.py
@@ -1,6 +1,6 @@
 class Solution(object):
-    def missingNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+	def missingNumber(self, nums):
+		"""
+		:type nums: List[int]
+		:rtype: int
+		"""
